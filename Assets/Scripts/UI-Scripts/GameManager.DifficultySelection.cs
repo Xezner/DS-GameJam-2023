9120b3f9463acfea004ad5562129d81fdb7b2365
duanematyu <rduane76@gmail.com>
2023-06-17 22:57:35 +0800

    Difficult Selection
    -Easy
    -Normal
    -Hard

--- Assets/Scripts/UI-Scripts/GameManager.DifficultySelection.cs
+++ Assets/Scripts/UI-Scripts/GameManager.DifficultySelection.cs
@@ -1,10 +1,26 @@
 
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public partial class GameManager
 {
       public static class DifficultySelection
     {
+        public static void EasyDifficulty()
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        public static void NormalDifficulty()
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        public static void HardDifficulty()
+        {
+            SceneManager.LoadScene(1);
+        }
+
         public static void BackToMainMenu()
         {
             Instance.Difficulty.SetActive(false);
