8096f16c06b959eaf73e1f02535ba2e08ce2e610
duanematyu <rduane76@gmail.com>
2023-06-18 01:18:20 +0800

    removed subclass
    adjusted the script of main menu and difficulty selection partial class

--- Assets/Scripts/UI-Scripts/GameManager.DifficultySelection.cs
+++ Assets/Scripts/UI-Scripts/GameManager.DifficultySelection.cs
@@ -4,27 +4,24 @@ using UnityEngine.SceneManagement;
 
 public partial class GameManager
 {
-      public static class DifficultySelection
+    public static void EasyDifficulty()
     {
-        public static void EasyDifficulty()
-        {
-            SceneManager.LoadScene(1);
-        }
+        SceneManager.LoadScene(1);
+    }
 
-        public static void NormalDifficulty()
-        {
-            SceneManager.LoadScene(1);
-        }
+    public static void NormalDifficulty()
+    {
+        SceneManager.LoadScene(1);
+    }
 
-        public static void HardDifficulty()
-        {
-            SceneManager.LoadScene(1);
-        }
+    public static void HardDifficulty()
+    {
+        SceneManager.LoadScene(1);
+    }
 
-        public static void BackToMainMenu()
-        {
-            Instance.Difficulty.SetActive(false);
-            Instance.MainMenuUI.SetActive(true);
-        }
+    public static void BackToMainMenu()
+    {
+        Instance.Difficulty.SetActive(false);
+        Instance.MainMenuUI.SetActive(true);
     }
 }
